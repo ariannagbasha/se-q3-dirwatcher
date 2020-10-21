9991a841e904f86b484f7a41c30a32ba9118350b
Arianna Basha <ariannagbasha@gmail.com>
2020-10-21 11:17:31 -0500

    basic functionality achieved with dirwatcher

--- dirwatcher.py
+++ dirwatcher.py
@@ -1,30 +1,65 @@
-#!/usr/bin/env python3
-"""
-Dirwatcher - A long-running program
-"""
 
-__author__ = "ariannagbasha a.k.a Gabby collabs: Sondos and Shanquel"
+
+__author__ = "Gabby and got Pete's help and got Tracy's help as well"
+
 
 import sys
 import signal
 import time
+import argparse
+import os
+import logging
+# Level(#'val)           When it’s used
+# DEBUG(10)          Detailed information, typically of interest only when
+#                    diagnosing problems.
+# INFO(20)           Confirmation that things are working as expected.
+# WARNING(30)        An indication that something unexpected happened, or
+#                    indicative of some problem in the near future
+#                    (e.g. ‘disk space low’).
+#                    The software is still working as expected.
+# ERROR(40)          Due to a more serious problem, the software has not been
+#                    able to perform some function.
+# CRITICAL(50)       A serious error, indicating that the program itself may be 
+#                    unable to continue running.
 
 exit_flag = False
+global_dict = {}
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+file_handler = logging.FileHandler('file.log')
+file_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 def search_for_magic(filename, start_line, magic_string):
-    
-    return
+    with open(filename) as f:
+        for line_num, line_string in enumerate(f):
+            if line_string.find(magic_string) != -1:
+                print(filename, line_num)
 
 
 def watch_directory(path, magic_string, extension, interval):
-    # Your code here
-    return
+    file_ls = os.listdir(path)
+    for file_name in file_ls:
+        search_for_magic(path + '/' + file_name, 0, magic_string)
 
 
 def create_parser():
-    # Your code here
-    return
+    parser = argparse.ArgumentParser(
+        description="Looking for changing files in directory.")
+    parser.add_argument('directory', help='filename(s) to parse')
+    parser.add_argument(
+        '-e', '--ext', help='extension input')
+    parser.add_argument(
+        '-i', '--int', default=1, type=float, help='polling interval period of time\
+                        between the end of the timeout period')
+    parser.add_argument(
+        '-t', '--txt', help='magic search')
+    return parser
 
 
 def signal_handler(sig_num, frame):
@@ -36,31 +71,42 @@ def signal_handler(sig_num, frame):
     :return None
     """
     # log the associated signal name
-    # logger.warn('Received ' + signal.Signals(sig_num).name)
+    global exit_flag
+    if signal.Signals(sig_num).name == 'SIGINT':
+        logger.warning('Received ' + signal.Signals(sig_num).name)
+    exit_flag = True
 
 
-def main():
+def main(args):
+    parser = create_parser()
+    ns = parser.parse_args(args)
     # Hook into these two signals from the OS
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
     # Now my signal_handler will get called if OS sends
     # either of these to my process.
 
+
     while not exit_flag:
         try:
             # call my directory watching function
+            watch_directory(ns.directory, ns.txt, ns.ext, ns.int)
+            time.sleep(ns.int)
             pass
         except Exception as e:
+            print(e)
             # This is an UNHANDLED exception
             # Log an ERROR level message here
             pass
 
+
         # put a sleep inside my while loop so I don't peg the cpu usage at 100%
-        # time.sleep(polling_interval)
+        # time.sleep(args.int)
 
     # final exit point happens here
     # Log a message that we are shutting down
     # Include the overall uptime since program start
+
 
 
 if __name__ == '__main__':
